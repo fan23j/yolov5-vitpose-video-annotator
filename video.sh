ff91da20054562aba5c6d18d636726d45047a205
Jack Fan <54330861+fan23j@users.noreply.github.com>
2023-04-07 09:23:26 -0400

    Update video.sh

--- video.sh
+++ video.sh
@@ -1,6 +1,5 @@
 python demo/top_down_video_demo_with_yolov5.py \
     --pose-config configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/halpe/ViTPose_huge_halpe_256x192.py \
     --pose-checkpoint PATH_TO_PRETRAINED_POSE_MODEL \
-    --det-checkpoint PATH_TO_PRETRAINED_DETECTOR \
     --video-path INPUT_VIDEO_PATH \
     --out-video-root OUTPUT_DIR \
